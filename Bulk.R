f4d655d993f181080f4ee3ca0268536264c25cd7
williamgromstad16 <william.esbjug.gromstad@nmbu.no>
2024-11-08 15:28:30 +0100

    Initial commit

--- Bulk.R
+++ Bulk.R
@@ -1,1 +1,7 @@
-# This is where I will analyse data from the bulk stable isotope analysis.
+library(usethis)
+use_git_config(
+  user.name = "williamgromstad16", 
+  user.email = "william.esbjug.gromstad@nmbu.no"
+)
+
+# This is where I will analyse data from the bulk stable isotope analysis. 
